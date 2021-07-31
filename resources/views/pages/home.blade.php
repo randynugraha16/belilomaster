cd40fc6003d6a3b1323f7801e5d8a5ee65873279
randynugraha16 <randynugraha11@gmail.com>
2021-07-31 18:12:02 +0700

    test branch

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -15,6 +15,9 @@
     <section class="store-carousel">
       <div class="container-fluid">
         <div class="row">
+          <div class="col-12">
+            <h1>Test Git Branch</h1>
+          </div>
           <div class="col-lg-12" style="height: auto">
             <div
               id="banner"
